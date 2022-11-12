de30757319b4dd61bd52d387b6a26db81c7a9ad0
MashaMl <yakovenko.mashka@gmail.com>
2022-11-12 17:57:18 +0200

    Name of course

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Masha Malko");
         System.out.println("***@gmail.com");
+        System.out.println("Git Java Course");
     }
 }
